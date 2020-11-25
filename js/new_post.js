cc5448f5d763973fd49e1f905830f79d994915cb
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2020-11-25 21:30:16 +0900

    commit

---
+++ js/new_post.js
@@ -0,0 +1,17 @@
+
+
+
+window.addEventListener('DONContentLoaded', function () {
+
+
+    const fileInput = document.querySelector('#id_photo');
+
+    fileInput.addEventListener('change', handleImage)
+
+
+
+
+
+
+
+})
